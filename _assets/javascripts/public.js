c86ac7c701891e5968d3e1252bbaf9e35ecc67b9
Brandon Arbini <b@arbini.com>
2015-03-18 23:24:18 -0700

    Ticketleap events.

--- _assets/javascripts/public.js
+++ _assets/javascripts/public.js
@@ -1,6 +1,46 @@
-angular.module("WayfareApp", []).config(['$interpolateProvider', function($interpolateProvider) {
+angular.module('truncate', [])
+.filter('characters', function () {
+    return function (input, chars, breakOnWord) {
+        if (isNaN(chars)) return input;
+        if (chars <= 0) return '';
+        if (input && input.length > chars) {
+            input = input.substring(0, chars);
+
+            if (!breakOnWord) {
+                var lastspace = input.lastIndexOf(' ');
+                //get last space
+                if (lastspace !== -1) {
+                    input = input.substr(0, lastspace);
+                }
+            }else{
+                while(input.charAt(input.length-1) === ' '){
+                    input = input.substr(0, input.length -1);
+                }
+            }
+            return input + '...';
+        }
+        return input;
+    };
+})
+.filter('words', function () {
+    return function (input, words) {
+        if (isNaN(words)) return input;
+        if (words <= 0) return '';
+        if (input) {
+            var inputWords = input.split(/\s+/);
+            if (inputWords.length > words) {
+                input = inputWords.slice(0, words).join(' ') + '...';
+            }
+        }
+        return input;
+    };
+});
+
+angular.module("WayfareApp", ['truncate']).config(['$interpolateProvider', function($interpolateProvider) {
   $interpolateProvider.startSymbol('{[').endSymbol(']}');
-}]).controller("ContactFormController", ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+}]);
+
+angular.module('WayfareApp').controller("ContactFormController", ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
   $scope.contactInfo = {}
   $scope.submit = function() {
     $http.post("/contact.json", $scope.contactInfo).success(function() {
@@ -12,5 +52,42 @@ angular.module("WayfareApp", []).config(['$interpolateProvider', function($inter
     }).error(function(response) {
       $scope.error = response.error;
     });
+  }
+}]);
+
+angular.module("WayfareApp").filter('linebreaks', function() {
+  return function(text) {
+    return text.replace(/\n/g, '<br>');
+  };
+});
+
+angular.module('WayfareApp').factory('TicketLeap', ['$http', function($http) {
+  return function() {
+    return $http.jsonp("http://public-api.ticketleap.com/organizations/wayfare/events?key=7331436096006273&page_size=50&callback=JSON_CALLBACK")
+  };
+}]);
+
+angular.module("WayfareApp").controller("EventsController", ['$scope', 'TicketLeap', function($scope, TicketLeap) {
+  $scope.events = [];
+  TicketLeap().success(function(data) {
+    $scope.events = data.events;
+  }).error(function() {
+    console.log("Error loading events");
+  });
+  $scope.convertToDate = function(date) {
+    return Date.parse(date);
+  }
+  $scope.sortDate = function(event) {
+    return Date.parse(event.earliest_start_local);
+  }
+  $scope.pastEvents = function(value, index) {
+    if ($scope.convertToDate(value.earliest_start_local) < (new Date())) {
+      return true;
+    }
+  }
+  $scope.futureEvents = function(value, index) {
+    if ($scope.convertToDate(value.earliest_start_local) >= (new Date())) {
+      return true;
+    }
   }
 }]);
